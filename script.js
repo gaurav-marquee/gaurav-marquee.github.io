391656a5b316c2d2bc8424c71a9ed87f2a2650cc
Gaurav Kumar <gaurav.marquee@gmail.com>
2023-01-31 16:21:10 +0530

    changes in form

--- script.js
+++ script.js
@@ -60,43 +60,48 @@ function toggleSideMenu() {
 }
 
 
-
-// $(document).ready(function(){
-//     $("#myForm").submit(function(){
-//         var firstName = $("#firstName").val();
-//         var lastName = $("#lastName").val();
-//         var email = $("#email").val();
-        
-//         if (firstName == ""){
-//             $("#fname_err").html("**First name is required.");
-//             $("#fname_err").css("color", "red");
-//         }
-
-//         if (lastName == ""){
-//             $("#lname_err").html("**Last name is required.");
-//             $("#lname_err").css("color", "red");
-//         }
-
-//         if (email == ""){
-//             $("#email_err").html("**The Email must be filled.");
-//             $("#email_err").css("color", "red");
-//         }
-//     });
-// });
+// const firstName = document.getElementById("firstName");
+// const fLabel = document.querySelector(".flabel");
+//     firstName.addEventListener("click", floatLabel);
+//     function floatLabel() {
+//         fLabel.classList.add("label-float");
+//     }
 
 
 
 $(document).ready(function(){
+    // $("#myForm").submit(function(){
+    //     var firstName = $("#firstName").val();
+    //     var lastName = $("#lastName").val();
+    //     var email = $("#email").val();
+        
+    //     if (firstName == ""){
+    //         $("#fname_err").html("**First name is required.");
+    //         $("#fname_err").css("color", "red");
+    //     }
+
+    //     if (lastName == ""){
+    //         $("#lname_err").html("**Last name is required.");
+    //         $("#lname_err").css("color", "red");
+    //     }
+
+    //     if (email == ""){
+    //         $("#email_err").html("**The Email must be filled.");
+    //         $("#email_err").css("color", "red");
+    //     }
+    // });
+
+
     $("#myForm").validate({
         rules: {
             firstName:{
                 required: true,
-                minlength: 1,
+                minlength: 3,
                 maxlength: 50
             },
             lastName:{
                 required: true,
-                minlength: 1,
+                minlength: 3,
                 maxlength: 50
             },
             email:{
@@ -108,23 +113,64 @@ $(document).ready(function(){
         },
         messages: {
             firstName:{
-                required: "**please enter your first name",
-                minlength: "**Name at least 1 characters",
-                maxlength: "**Please enter between 2 and 50 characters" 
+                required: "**please enter your first name.",
+                minlength: "**Name at least 3 characters.",
+                maxlength: "**Please enter between 3 and 50 characters." 
             },
             lastName:{
-                required: "**please enter your last name",
-                minlength: "**Name at least 1 characters",
-                maxlength: "**Please enter between 2 and 50 characters" 
+                required: "**please enter your last name.",
+                minlength: "**Name at least 3 characters.",
+                maxlength: "**Please enter between 3 and 50 characters." 
             },
             email:{
-                required: "**please enter your email address",
-                email: "**please enter valid email address"
+                required: "**please enter your email address.",
+                email: "**please enter valid email address."
             },
-            subject: "**please enter subject",
-            message: "**please write messages"
-        }
+            subject: "**please enter subject.",
+            message: "**please write messages."
+        },
+        errorPlacement: function(error, element) {
+            error.insertBefore(".form-details");
+        },
+       
+        // errorClass: "form_validation_error",  
+        errorLabelContainer: $("#error-note")
     });
 
-    // $("a.active").parents('input, textarea').css("outline", "1px solid #dc3545");
+
+    // $("#firstName").click(function(){
+    //     $(".flabel").css("transform", "translateY(-0.7em)");
+    // });
+
+    var formFields = $('.form-field');
+    
+    formFields.each(function() {
+      var field = $(this);
+      var input = field.find('.form-input');
+      var label = field.find('label');
+      
+      function checkInput() {
+        var valueLength = input.val().length;
+        
+        if (valueLength > 0 ) {
+          label.addClass('freeze')
+        } else {
+              label.removeClass('freeze')
+        }
+      }
+      
+      input.change(function() {
+        checkInput()
+      })
+    });
+    
+
+
+
+
+   
 });
+
+$(document).ready(function() {
+    
+  });
